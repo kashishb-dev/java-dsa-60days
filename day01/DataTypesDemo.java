a2192688eb25b7dcec2ae355eab913b4051c011b
kashishb-dev <sunset.kashish@gmail.com>
2025-08-31 22:02:44 +0530

    Add day01 programs

---
+++ day01/DataTypesDemo.java
@@ -0,0 +1,16 @@
+// Demonstration of primitive and non-primitive data types in Java
+public class DataTypesDemo {
+    public static void main(String[] args) {
+        int age = 30;
+        double price = 19.99;
+        char initial = 'J';
+        boolean isStudent = true;
+        String name = "John Doe";
+
+        System.out.println("Age: " + age);
+        System.out.println("Price: $" + price);
+        System.out.println("Initial: " + initial);
+        System.out.println("Is student: " + isStudent);
+        System.out.println("Name: " + name);
+    }
+}
